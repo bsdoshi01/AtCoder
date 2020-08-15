e917c0074916e02ba4e8c4f7ecccaa7f22adeedf
bsdoshi01 <bsdoshi01@gmail.com>
2020-08-15 15:33:40 +0530

    Multiplication 3

---
+++ Beginner Contest 169/c.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+void solve() {
+	ll num1;
+	ld num2;
+	ld ans;
+	cin >> num1 >> num2;
+	ans = num1 * num2;
+	cout << (ll)ans << endl;
+}
+
+int main() {
+	solve();
+
+	return 0;
+}
