e68732f723b8bc46bde5144ca59c1f3b2de843df
bsdoshi01 <bsdoshi01@gmail.com>
2020-08-15 15:29:27 +0530

    Multiplication 1

---
+++ Beginner Contest 169/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve() {
+	int a, b;
+	cin >> a >> b;
+	cout << a*b << endl;
+}
+
+int main() {
+	solve();
+
+	return 0;
+}
