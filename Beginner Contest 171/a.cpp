ac2d241563b89cf84b291f029e0ccbbc4146d041
bsdoshi01 <bsdoshi01@gmail.com>
2020-08-16 10:58:40 +0530

    Alphabet

---
+++ Beginner Contest 171/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+
+void solve() {
+	char ch;
+	cin >> ch;
+	if ((int)ch >= 65 && (int)ch <= 90) 
+		cout << "A" << endl;
+	else 
+		cout << "a" << endl;
+}
+
+int main() {
+	solve();
+	return 0;
+}
