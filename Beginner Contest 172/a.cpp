cef89109c54d066cb2120e1c0cf3f2bd4d5c0378
bsdoshi01 <bsdoshi01@gmail.com>
2020-08-16 10:40:25 +0530

    Calc

---
+++ Beginner Contest 172/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+
+void solve() {
+	int a;
+	cin >> a;
+	cout << (a + a*a + a*a*a) << endl;
+}
+
+int main() {
+	solve();
+	return 0;
+}
